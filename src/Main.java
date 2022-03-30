224aa115b6597e2b83386624d70ef46f45395d64
EscapeFromJava <reske.artur@gmail.com>
2022-03-30 21:10:18 +0300

    v 1.1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("¬ведите кол-во элементов в массиве");
